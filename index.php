a90e76a5f6b923057b96a42ad06bfb869aa93fa5
ChallengerC4 <ChallengerC4@users.noreply.github.com>
2015-05-24 13:45:01 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<pre>
+<?php
+require_once("notes_definition.php");
+
+// anything here is done by server
+echo "this is a test <BR>";
+
+// global variables here
+$key="C";
+
+$notes=array();
+
+$notes  = array(newc4(), newd4(), newe4(), newc4(), newc4(), newa3(), newb3(), newc4());
+$notes2 = array();
+foreach ($notes as $n) {
+    $n=$n->downsixth();
+    array_push($notes2, $n);
+}
+echo "notes2:";
+print_r($notes2);
+die();
+?>
+</pre>
+
+<!-- All html below -->
+
+
+<html>
+    <head>
+        <title>Welcome to composition</title>
+    </head>    
+    <body>
+        <form>
+            <?php
+            for ($i=0;$i<10;$i=$i+1) {
+                ?>
+                <input type="button" value="Button <?php echo $i?>">
+                <?
+            }
+            ?>
+            <input type="submit" value="I am a button." />
+        </form>
+        
+    </body>
+
+<!-- this is out of the server -->
+
+</html>
